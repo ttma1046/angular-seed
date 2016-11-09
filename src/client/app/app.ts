c7c7a55192e4fe5e16265fa14b095fe104a438e6
Chandler Fang <chandlerfang@gmail.com>
2016-11-09 22:20:23 +1100

    Start animation guide.

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -12,27 +12,43 @@ const clickAdd$ = Observable.fromEvent<MouseEvent>($add, 'click');
 
 const input$ = enter$.merge(clickAdd$);
 
-const app$ = input$
-	.map(() => $input.value)
-	.filter(r => r !== '')
-	.map(createTodoItem)
-	.do((ele: HTMLLIElement) => { 
-		$list.appendChild(ele);
-		$input.value = '';
-	})
-	.mergeMap($todoItem => {
-		return Observable.fromEvent<MouseEvent>($todoItem, 'click')
-		.filter(e => e.target === $todoItem)
-		.mapTo($todoItem);
-	})
-	.do(($todoItem: HTMLElement) => {
-		if ($todoItem.classList.contains('done')) {
-			$todoItem.classList.remove('done');
-		} else {
-			$todoItem.classList.add('done');
-		}
-	})
-	.do(r => console.log(r));
+const item$ = input$
+  .map(() => $input.value)
+  .filter(r => r !== '')
+  .map(createTodoItem)
+  .do((ele: HTMLLIElement) => {
+    $list.appendChild(ele)
+    $input.value = ''
+  })
+  .publishReplay(1)
+  .refCount();
+
+const toggle$ = item$.mergeMap($todoItem => {
+  return Observable.fromEvent<MouseEvent>($todoItem, 'click')
+    .filter(e => e.target === $todoItem)
+    .mapTo($todoItem);
+})
+  .do(($todoItem: HTMLElement) => {
+    if ($todoItem.classList.contains('done')) {
+      $todoItem.classList.remove('done');
+    } else {
+      $todoItem.classList.add('done');
+    }
+  });
+
+const remove$ = item$.mergeMap($todoItem => {
+const $removeButton = $todoItem.querySelector('.button-remove');
+  return Observable.fromEvent($removeButton, 'click')
+    .mapTo($todoItem);
+})
+  .do(($todoItem: HTMLElement) => {
+    // 从 DOM 上移掉 todo item
+    const $parent = $todoItem.parentNode;
+    $parent.removeChild($todoItem);
+  });
+
+const app$ = toggle$.merge(remove$)
+  .do(r => console.log(r));
 
 app$.subscribe();
 
