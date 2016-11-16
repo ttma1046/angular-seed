f72cc6ddfe6b0db34985ce1863de330b1a5cf04f
Chandler Fang <chandlerfang@gmail.com>
2016-11-17 09:29:17 +1100

    Add Http client page for.

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms'
 
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(routes), AboutModule, HomeModule, SharedModule.forRoot()],
+  imports: [BrowserModule, HttpModule, FormsModule, JsonpModule, RouterModule.forRoot(routes), AboutModule, HomeModule, SharedModule.forRoot()],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
